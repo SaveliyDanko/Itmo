28bb63efc6818c5686fbe708038747caa50670d6
SaveliyDanko <dankosaveliy.m@gmail.com>
2023-12-12 19:44:11 +0300

    lab4v2.0

--- Programming/lab_4prog/src/places/Town.java
+++ Programming/lab_4prog/src/places/Town.java
@@ -1,23 +1,2 @@
-package places;
-
-public class Town extends Place{
-    private final Hotel hotel = new Hotel("Hotel");
-    public Town(String name) {
-        super(name);
-    }
-
-    public String openHotel(){
-        return hotel.toString();
-    }
-
-    static class Hotel{
-        private final String name;
-
-        public Hotel(String name){
-            this.name = name;
-        }
-        public String toString(){
-            return name;
-        }
-    }
+package places;public class Town {
 }
